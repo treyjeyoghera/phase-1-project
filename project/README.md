571b4fbbcdfd3baf4c797c775478c80fde6e788b
treyjeyoghera <treyjeyogheraeugene@gmail.com>
2024-04-15 08:37:41 +0300

    finish

---
+++ project/README.md
@@ -0,0 +1,27 @@
+# my project
+
+#### Date, 2024/04/12
+
+#### By *TREYJEY OGHERA*
+
+## Description
+It is an app like that uses fetch method to get the book that someone searches and gives her/him all boooks of that name and authors 
+
+## Installation
+You use git clone to be able to download the documents in the GitHub
+
+## Installation Requirements
+Git
+### Installation instruction
+```
+## Technologies used
+Github
+Javascript
+
+## Support and contact details
+github.com/treyjey-oghera
+
+
+
+
+
---
+++ project/index.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const searchButton = document.getElementById("searchButton");
+  const clearButton = document.getElementById("clearButton");
+
+  searchButton.addEventListener("click", searchBooks);
+  clearButton.addEventListener("click", clearSearchResults);
+
+  function searchBooks() {
+      const searchQuery = document.getElementById("searchInput").value;
+      const apiUrl = `https://openlibrary.org/search.json?q=${encodeURIComponent(searchQuery)}`;
+
+      fetch(apiUrl)
+          .then(response => {
+              if (!response.ok) {
+                  throw new Error('Network response was not ok');
+              }
+              return response.json();
+          })
+          .then(data => {
+              const searchResultsDiv = document.getElementById("searchResults");
+              searchResultsDiv.innerHTML = "";
+
+              data.docs.forEach(book => {
+                  const bookTitle = book.title;
+                  const bookAuthor = (book.author_name && book.author_name.length > 0) ? book.author_name[0] : "Unknown author";
+                  const bookKey = book.key;
+                  const bookURL = `https://openlibrary.org${bookKey}`;
+
+                  const bookHTML = `<p><strong>Title:</strong> <a href="${bookURL}" target="_blank">${bookTitle}</a></p><p><strong>Author:</strong> ${bookAuthor}</p>`;
+                  searchResultsDiv.insertAdjacentHTML("beforeend", bookHTML);
+              });
+          })
+          .catch(error => {
+              console.error('There was a problem with the fetch operation:', error);
+          });
+  }
+
+  function clearSearchResults() {
+      document.getElementById("searchResults").innerHTML = "";
+  }
+});
